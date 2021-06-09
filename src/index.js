0ba8949c9404da470f6fed832d80d8c8639aab06
Edwin Webb <edwin.webb@gmail.com>
2021-06-09 12:45:29 -0700

    add debug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+console.log('PUBLIC_URL', process.env.PUBLIC_URL)
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
